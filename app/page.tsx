3c40e6c4b3f841aab1cb16e52071c2f2adec9223
Jake <jakefernandez2016@gmail.com>
2024-09-17 16:02:06 -0700

    Basic sign up/sign in UI for the home screen using shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"; // Import the Button component
+import { Dashboard } from "@/components/authentication-04"; // Import the Dashboard component
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="relative">
+      <Dashboard /> {/* Render the Dashboard component */}
     </div>
   );
 }
